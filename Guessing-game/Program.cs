1676d62ad147eb1655b9a6dada9e550cb82f10a0
AmorimY <rafael.amorim.praia@gmail.com>
2023-07-09 10:44:16 -0300

    first commmit

---
+++ Guessing-game/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Guess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escolha um numero entre 1 - 100: ");
+           int numeroEscolhido = Convert.ToInt32(Console.ReadLine());
+
+            Random rdn = new Random();
+            int numeroSorteado = rdn.Next(1, 5);
+            int tentativas = 0;
+
+
+            while (numeroEscolhido != numeroSorteado)
+            {
+                if(numeroEscolhido > numeroSorteado)
+                {
+                    Console.WriteLine("numero errado, " + numeroEscolhido + " muito alto!!");
+                    numeroEscolhido = Convert.ToInt32(Console.ReadLine());
+                    tentativas++;
+                }
+                else if(numeroEscolhido < numeroSorteado)
+                {
+                    Console.WriteLine("numero errado, " + numeroEscolhido + " muito baixo!!");
+                    numeroEscolhido = Convert.ToInt32(Console.ReadLine());
+                    tentativas++;
+
+                }
+
+
+            }
+            Console.WriteLine("Voce acertou com " + tentativas + " tentativas");
+        }
+    }
+}
